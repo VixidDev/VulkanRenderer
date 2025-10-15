c4a62358ee25a5df023cea315502faece09ae423
VixidDev <52578495+VixidDev@users.noreply.github.com>
2025-10-15 21:03:56 +0100

    Remove note

--- main/gui/ImGUI.cpp
+++ main/gui/ImGUI.cpp
@@ -209,11 +209,7 @@ void GUI::draw() {
 			ImGui::EndTabItem();
 		}
 		if (ImGui::BeginTabItem("Effects")) {
-			ImGui::Text("Descriptor Sets: %d", descriptorSetCount);
 			ImGui::Text("Pre Processing Effects");
-			// TODO: FIX: Repeatedly enabling / disabling this causes ERROR_OUT_OF_POOL_MEMORY
-			// from vkAllocatorDescriptorSets. (Is it a SSAO specific thing or a more
-			// prevalent problem with swapchain recreation?)
 			if (ImGui::Checkbox("SSAO", &renderer.ssaoEnabled)) {
 				renderer.setRecreateSwapchain(true, true);
 			}
